326f6781101375394f9c4a5ce25ef347bdbd38fc
Henry Hsu <h_hsu817@yahoo.com>
2022-04-04 12:12:15 -0700

    Separated code into functions

--- dicts-word-count/wordcount.py
+++ dicts-word-count/wordcount.py
@@ -1,22 +1,32 @@
 """Count words in file."""
 
 import sys
-word_count = {}
 
-try:
-    input_file = open(sys.argv[1])
-except IndexError:
-    print ("Please provide a file to process")
-    exit()
-except FileNotFoundError:
-    print ("invalid filename")
-    exit()
+def main():
+    words = read_input()
+    print_output(words)
 
-for line in input_file:
-    line = line.rsplit()
-    for word in line:
-        word_count[word] = word_count.get(word, 0) + 1
+def read_input():
+    word_count = {}
+    try:
+        input_file = open(sys.argv[1])
+    except IndexError:
+        print ("Please provide a file to process")
+        exit()
+    except FileNotFoundError:
+        print ("invalid filename")
+        exit()
 
-for key, value in word_count.items():
-    print (key, value)
+    for line in input_file:
+        line = line.rsplit()
+        for word in line:
+            word_count[word] = word_count.get(word, 0) + 1
+    
+    return (word_count)
 
+def print_output(word_count):
+    for key, value in word_count.items():
+        print (key, value)
+
+
+main()
