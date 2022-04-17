cc0141c445b71cb435a1600e67218dcb6d1ae145
Henry Hsu <h_hsu817@yahoo.com>
2022-04-17 11:37:42 -0700

    Implements 'wordsInCommon' function

--- js-trials-2/furtherStudy.js
+++ js-trials-2/furtherStudy.js
@@ -1,8 +1,15 @@
 'use strict';
 
 function wordsInCommon(words1, words2) {
-  // Replace this with your code
+  const answer = new Set;
+  
+  words1.forEach(word => {
+    if (words2.includes(word)) { answer.add(word) };
+  })
+
+  return Array.from(answer)
 }
+
 
 function kidsGame(names) {
   // Replace this with your code
