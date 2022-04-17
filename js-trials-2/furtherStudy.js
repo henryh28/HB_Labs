d10e1ca57e38aa2283ac5748c085a2281e53d15a
Henry Hsu <h_hsu817@yahoo.com>
2022-04-17 12:21:31 -0700

    Implements 'kids_game' function

--- js-trials-2/furtherStudy.js
+++ js-trials-2/furtherStudy.js
@@ -11,6 +11,29 @@ function wordsInCommon(words1, words2) {
 }
 
 
-function kidsGame(names) {
-  // Replace this with your code
+function kids_game(words) {
+
+  if (words.length == 0) {
+    return 0;
+  }
+  
+  const answer = [words.shift()];
+  const words_starting_with_letter = {}
+
+  words.forEach(word => {
+    word[0] in words_starting_with_letter ? words_starting_with_letter[word[0]].push(word) : words_starting_with_letter[word[0]] = [word]
+  })
+
+  while (true) {
+    let starting_letter = answer[answer.length-1].slice(-1)
+    
+    if (starting_letter in words_starting_with_letter) {
+      if (words_starting_with_letter[starting_letter].length == 0) {
+        return answer
+      }
+      answer.push(words_starting_with_letter[starting_letter].shift());
+    } else {
+      return (answer);
+    }
+  }  
 }
