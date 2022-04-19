a61e104efbc5e35e8b9f1c087e556e30b5b1b4e4
Henry Hsu <h_hsu817@yahoo.com>
2022-04-18 18:02:25 -0700

    Implements UI for application per requirements

--- js-sharkwords-ui/static/js/sharkwords.js
+++ js-sharkwords-ui/static/js/sharkwords.js
@@ -20,13 +20,21 @@ const numWrong = 0;
 // Loop over the chars in `word` and create divs.
 // The divs should be appended to the section with id="word-container".
 const createDivsForChars = (word) => {
-  // Replace this with your code
+  for (const char of word) {
+    const container=document.querySelector('#word-container');
+    container.insertAdjacentHTML("beforeend", `<div class="letter-box ${char}"></div>`);
+  }
+
 };
 
 // Loop over each letter in the alphabet and generate a button for each letter
 // The buttons should be appended to the section with id="letter-buttons"
 const generateLetterButtons = () => {
-  // Replace this with your code
+  const buttons=document.querySelector("#letter-buttons")
+  
+  for (const char of ALPHABET) {
+    buttons.insertAdjacentHTML("beforeend", `<button>${char}</button>`);
+  }
 };
 
 // Set the `disabled` property of `buttonEl` to `true.
@@ -34,16 +42,22 @@ const generateLetterButtons = () => {
 // `buttonEl` is an `HTMLElement` object.
 //
 const disableLetterButton = (buttonEl) => {
-  // Replace this with your code
+  buttonEl.disabled="true";
 };
+
 
 // This is a helper function we will use in the future
 // It should return `true` if `letter` is in the word
 // For now, you should test it out to make sure it works
 
 const isLetterInWord = (letter) => {
-  // Replace this with your code
+
+  button = document.querySelector(`div.${letter}`)
+
+  return button!=null;
 };
+
+
 
 // This is like if __name__ == '__main__' in Python
 // It will be called when the file is run (because
@@ -55,12 +69,10 @@ const isLetterInWord = (letter) => {
   // and can tell if things look correct for this word
   const word = 'hello';
 
-  // call the function that makes an empty line for each letter in the word
-  // Replace this line with the function call
-
-  // call the function that makes a button for each letter in the alphabet
-  // Replace this line with the function call
+  createDivsForChars(word);
+  generateLetterButtons();
 
   // in the next lab, you will be adding functionality to handle when
   // someone clicks on a letter
 })();
+
