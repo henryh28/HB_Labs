fc8390828dbb215abd56b17d5a2014fe36939fb0
Henry Hsu <h_hsu817@yahoo.com>
2022-04-20 10:50:27 -0700

    Converts function to async/await functions

--- ajax/static/ajax-exercise.js
+++ ajax/static/ajax-exercise.js
@@ -1,40 +1,27 @@
 'use strict';
 
 // PART 1: SHOW A FORTUNE
-function showFortune(evt) {
-  fetch('/fortune')
-    .then(response => response.text())
-    .then(fortune => document.querySelector("#fortune-text").innerHTML = fortune);
+async function showFortune(evt) {
+  const response = await fetch('/fortune');
+  const fortune = await response.text();
+  document.querySelector("#fortune-text").innerHTML = fortune;
 }
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
 
 // PART 2: SHOW WEATHER
-function showWeather(evt) {
+async function showWeather(evt) {
   evt.preventDefault();
   const url = '/weather.json';
   const zipcode = document.querySelector('#zipcode-field').value;
 
-  fetch(`${url}?zipcode=${zipcode}`)
-    .then(response => response.json())
-    .then(jsonData => {
-      document.querySelector("#weather-info").innerText = jsonData.forecast;
-    })
+  const response = await fetch(`${url}?zipcode=${zipcode}`)
+  const weatherInfo = await response.json()
+  document.querySelector("#weather-info").innerText = weatherInfo.forecast;
 }
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
 // PART 3: ORDER MELONS
-function showResults(response) {
-  if (response.code == 'OK') {
-    document.querySelector('#order-status').classList.remove('order-error');
-    document.querySelector('#order-status').innerHTML = `<p>${response.msg}</p>`;
-  } else {
-    document.querySelector('#order-status').classList.add('order-error');
-    document.querySelector('#order-status').innerHTML = `<p><b>${response.msg}</b></p>`;    
-  }
-}
-
-
-function orderMelons(evt) {
+async function orderMelons(evt) {
   evt.preventDefault();
 
   const formData = {
@@ -47,15 +34,23 @@ function orderMelons(evt) {
     headers: { 'content-type': 'application/json' },
     body: JSON.stringify(formData)
   }
-  
-  fetch('/order-melons.json', requestOptions)
-  .then(response => response.json())
-  .then(showResults)
-  
 
-  // Reference: https://jasonwatmore.com/post/2021/09/05/fetch-http-post-request-examples
+//  const melonData = await (await fetch('/order-melons.json', requestOptions)).json()
+  const response = await fetch('/order-melons.json', requestOptions)
+  const melonData = await response.json()
+
+  if (melonData.code == 'OK') {
+    document.querySelector('#order-status').classList.remove('order-error');
+    document.querySelector('#order-status').innerHTML = `<p>${melonData.msg}</p>`;
+  } else {
+    document.querySelector('#order-status').classList.add('order-error');
+    document.querySelector('#order-status').innerHTML = `<p><b>${melonData.msg}</b></p>`;    
+  }
 }
+
 document.querySelector('#order-form').addEventListener('submit', orderMelons);
+// Reference: https://jasonwatmore.com/post/2021/09/05/fetch-http-post-request-examples
+// https://stackoverflow.com/a/64165114
 
 
 function displayPuppy(dogData) {
@@ -66,11 +61,16 @@ function displayPuppy(dogData) {
   }
 }
 
-function getDog () {
+async function getDog () {
   const url = "https://dog.ceo/api/breeds/image/random"
-  fetch(url)
-  .then(response => response.json())
-  .then(displayPuppy)
+  const response = await fetch(url)
+  const puppyData = await response.json()
+
+  if (puppyData.status == "success") {
+    document.querySelector("#dog-image").insertAdjacentHTML("beforebegin", `<img src=${puppyData.message} alt="woof"></img>`)
+  } else {
+    console.log ("Implement error handling here")
+  }
 }
 
 document.querySelector('#get-dog-image').addEventListener('click', getDog);
