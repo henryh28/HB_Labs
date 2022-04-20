d7a82941e8e387de00d9e56307eff2bf868d21c9
Henry Hsu <h_hsu817@yahoo.com>
2022-04-20 09:35:01 -0700

    Implements further study task using 'dog api'

--- ajax/static/ajax-exercise.js
+++ ajax/static/ajax-exercise.js
@@ -6,12 +6,9 @@ function showFortune(evt) {
     .then(response => response.text())
     .then(fortune => document.querySelector("#fortune-text").innerHTML = fortune);
 }
-
-
 document.querySelector('#get-fortune-button').addEventListener('click', showFortune);
 
 // PART 2: SHOW WEATHER
-
 function showWeather(evt) {
   evt.preventDefault();
   const url = '/weather.json';
@@ -22,13 +19,10 @@ function showWeather(evt) {
     .then(jsonData => {
       document.querySelector("#weather-info").innerText = jsonData.forecast;
     })
-
 }
-
 document.querySelector('#weather-form').addEventListener('submit', showWeather);
 
 // PART 3: ORDER MELONS
-
 function showResults(response) {
   if (response.code == 'OK') {
     document.querySelector('#order-status').classList.remove('order-error');
@@ -60,8 +54,26 @@ function orderMelons(evt) {
   
 
   // Reference: https://jasonwatmore.com/post/2021/09/05/fetch-http-post-request-examples
-
-  // TODO: show the result message after your form
-  // TODO: if the result code is ERROR, make it show up in red (see our CSS!)
 }
 document.querySelector('#order-form').addEventListener('submit', orderMelons);
+
+
+function displayPuppy(dogData) {
+  if (dogData.status == "success") {
+    document.querySelector("#dog-image").insertAdjacentHTML("beforebegin", `<img src=${dogData.message} alt="woof"></img>`)
+  } else {
+    console.log ("Implement error handling here")
+  }
+}
+
+function getDog () {
+  const url = "https://dog.ceo/api/breeds/image/random"
+  fetch(url)
+  .then(response => response.json())
+  .then(displayPuppy)
+}
+
+document.querySelector('#get-dog-image').addEventListener('click', getDog);
+
+
+
