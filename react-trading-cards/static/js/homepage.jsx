c3eccffffe8653f63cacc262f1c47c396ed0345b
Henry Hsu <h_hsu817@yahoo.com>
2022-04-20 20:13:37 -0700

    Implements ability to display collection of card objects using React

---
+++ react-trading-cards/static/js/homepage.jsx
@@ -0,0 +1,20 @@
+'use strict';
+
+function Homepage() {
+
+
+
+  return (
+          <React.Fragment>
+            <div>Welcome user!</div>
+            <div id="balloonicorn_logo">
+              <img src="static/img/balloonicorn.jpg"></img>
+            </div>
+            <p><a href="/cards">Go to cards</a></p>
+          </React.Fragment>
+);
+
+
+}
+
+ReactDOM.render(<Homepage />, document.querySelector('#app'));
