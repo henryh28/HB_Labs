b366d1586e902d5d862b1ef64f609751e47733e8
Henry Hsu <h_hsu817@yahoo.com>
2022-04-26 13:51:30 -0700

    Implements routes and functions to handle displaying 'Movie' & 'User' records

--- ratings-v2/crud.py
+++ ratings-v2/crud.py
@@ -4,14 +4,37 @@ from model import db, User, Movie, Rating, connect_to_db
 from datetime import datetime as dt
 
 
+# ========= User related functions ==========
 def create_user(email, password):
     """Create and return a new user."""
     return User(email=email, password=password)
 
+
+def get_all_users():
+    """ Return a list containing all users in database """
+    return User.query.all()
+
+def get_single_user(user_id):
+    """ Return info pertaining to specified user """
+    return User.query.get(user_id)
+
+
+
+# ========= Movie related functions ==========
 def create_movie(title, overview, release_date, poster_path):
     """ Create and return a new movie """
     return Movie(title = title, overview = overview, release_date = release_date, poster_path = poster_path)
 
+
+def get_all_movies():
+    """ Return a list containing all movies in database """
+    return Movie.query.all()
+
+def get_single_movie(movie_id):
+    """ Return info pertaining to specified movie """
+    return Movie.query.get(movie_id)
+
+# ========= Rating related functions ==========
 def create_rating(score, user, movie):
     """ Create and return a new rating """
     return Rating(score = score, user = user, movie = movie)
--- ratings-v2/server.py
+++ ratings-v2/server.py
@@ -1,14 +1,66 @@
 """Server for movie ratings app."""
 
-from flask import Flask
+from urllib import response
+from flask import Flask, render_template, request, flash, session, redirect
+from model import connect_to_db, db
+import crud
+from jinja2 import StrictUndefined
+
 
 app = Flask(__name__)
+app.secret_key = "dev"
+app.jinja_env.undefined = StrictUndefined
 
 
-# Replace this with routes and view functions!
+
+@app.route("/")
+def homepage():
+    """ View Homepage """
+
+    return render_template("homepage.html")
+
+
+# Movie related routes
+@app.route("/movies")
+def get_all_movies():
+    """ Displays all movies in database """
+
+    all_movies = crud.get_all_movies()
+
+    return render_template("all_movies.html", all_movies = all_movies)
+
+
+@app.route("/movies/<movie_id>")
+def view_movie_detail(movie_id):
+    """ Display info for specified movie """
+
+    movie = crud.get_single_movie(movie_id)
+
+    return render_template("movie_details.html", movie = movie)
+
+
+
+# User related routes
+@app.route("/users")
+def get_all_users():
+    """ Displays all users in database """
+
+    all_users = crud.get_all_users()
+
+    return render_template("all_users.html", all_users = all_users)
+
+
+@app.route("/users/<user_id>")
+def view_user_detail(user_id):
+    """ Display info for specified user """
+
+    user = crud.get_single_user(user_id)
+
+    return render_template("user_details.html", user = user)
+
 
 
 if __name__ == "__main__":
     # DebugToolbarExtension(app)
-
+    connect_to_db(app)
     app.run(host="0.0.0.0", debug=True)
