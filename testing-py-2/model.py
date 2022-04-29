c17e518786e5dd02bc48c7caf9d04af9e93e1dba
Henry Hsu <h_hsu817@yahoo.com>
2022-04-28 17:31:49 -0700

    Adds test

--- testing-py-2/model.py
+++ testing-py-2/model.py
@@ -12,7 +12,7 @@ class Game(db.Model):
     description = db.Column(db.String(100))
 
 
-def connect_to_db(app, db_uri="postgresql:///games"):
+def connect_to_db(app, db_uri="postgresql:///testdb"):
     app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
     db.app = app
     db.init_app(app)
@@ -20,9 +20,14 @@ def connect_to_db(app, db_uri="postgresql:///games"):
 
 def example_data():
     """Create example data for the test database."""
-    # FIXME: write a function that creates a game and adds it to the database.
-    print("FIXME")
 
+    db.session.add(Game(name = "Wingspan", description = "Attract a beautiful and diverse collection of birds to your wildlife reserve."))
+    db.session.add(Game(name = "Viticulture", description = "Create the most prosperous winery in Italy from the Tuscan vineyard you've inherited."))
+    db.session.add(Game(name = "Scoville", description = "A plunge into a pepper planters plan, strategize your field to win the chili contest!"))
+#    db.session.add(Game(name = "Wingspan", description = "Attract a beautiful and diverse collection of birds to your wildlife reserve."))
+#    db.session.add(Game(name = "Marvel Champions - The Card Game", description = "Battle Marvel villains with unique teams of iconic heroes in this LCG."))
+    db.session.commit()
+    
 
 if __name__ == '__main__':
     from party import app
